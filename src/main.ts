6e74871f9c300d28f348d47935bb198fda724c39
NahidJHN <jubaidhossain66@gmail.com>
2023-10-29 20:11:18 +0600

    manage cors url

--- src/main.ts
+++ src/main.ts
@@ -18,12 +18,10 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
-    // httpsOptions: {
-    //   key: fs.readFileSync(path.join(__dirname, "..", "cert.key")),
-    //   cert: fs.readFileSync(path.join(__dirname, "..", "cert.crt")),
-    // },
   });
-  app.enableCors({ origin: "*" });
+  app.enableCors({
+    origin: ["https://chat-app-fe-lovat.vercel.app/", "http://localhost:3000"],
+  });
   app.setGlobalPrefix("api/v1"); //route prefix
   app.useGlobalPipes(
     new ValidationPipe({
